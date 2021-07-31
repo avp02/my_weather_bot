8afb225e585a53e20e9304d54880aec6217f5001
avp02 <67960868+avp02@users.noreply.github.com>
2021-07-31 12:07:18 +0300

    Add files via upload

---
+++ src/test/java/by/avp/weatherbot/WeatherbotApplicationTests.java
@@ -0,0 +1,13 @@
+package by.avp.weatherbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
